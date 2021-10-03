ea1208fbd3a2b3e0295dd8d283d1456c554db9a4
Seyed Saeid Dehghani <s.saeid.dehghani@gmail.com>
2021-10-03 19:01:58 +0330

    Update documents

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+sys.setrecursionlimit(1500)
 
 
 # -- Project information -----------------------------------------------------
@@ -24,14 +25,24 @@ author = 'Seyed Saeid Dehghani'
 # The full version, including alpha/beta/rc tags
 release = '1.1'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'rinoh.frontend.sphinx'
+
 ]
+
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '10pt',
+    'preamble': '',
+    'figure_align': 'htbp'
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,13 +59,15 @@ language = 'En'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+
+# EPUB options
+epub_show_urls = 'footnote'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,18 +3,16 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Voipy's documentation!
+Welcome to VoiPy's documentation!
 =================================
 
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
 
+sip
+===================
+.. automodule:: VoiPy.sip
+   :members:
 
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+rtp
+===================
+.. automodule:: VoiPy.rtp
+   :members:
